c1634ab56f2d663b2702aef6f1548614f76ea077
zeketx <zekemauricio@icloud.com>
2019-06-19 16:55:39 -0500

    added vuetify entry points

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
+
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
