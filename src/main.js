5dac8a1e0772e108e2a5a9cefaa0326cee6962f6
zeketx <zekemauricio@icloud.com>
2019-06-19 22:49:53 -0500

    added font awesome icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faGooglePlusSquare, faFreeCodeCamp} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add( faGithub, faLinkedin, faEnvelope, faGooglePlusSquare, faFreeCodeCamp, faLink)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
 Vue.use(Vuetify)
